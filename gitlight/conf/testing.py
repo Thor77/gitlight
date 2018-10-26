2a88ff0b30b7f126cdb3bfb2744180fff16ea2f0
Michael Lustfield <michael@lustfield.net>
2018-10-26 18:07:46 -0500

    Clean up issues found by codacy.

--- gitlight/conf/testing.py
+++ gitlight/conf/testing.py
@@ -3,4 +3,4 @@ import os
 DEBUG=False
 TESTING=True
 DB_ENGINE=os.environ.get('DB_ENGINE', 'sqlite3')
-DB_FILE=os.environ.get('DB_FILE', '/tmp/gitlight.sql')
+DB_FILE=os.environ.get('DB_FILE', '/var/lib/gitlight/data.sqlite3')
